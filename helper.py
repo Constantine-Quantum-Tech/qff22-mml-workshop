a5c2b6bd97312a4e59c94503b2f827085af703c6
Mohamed <mm.louamri@gmail.com>
2022-10-20 20:50:23 +0100

    feat: add the gates part
    - Adding more images to the QPU as coprocessor part
    - Wrote the Gates part

--- helper.py
+++ helper.py
@@ -1,5 +1,12 @@
 from qiskit import Aer, transpile
+from qiskit.visualization import plot_bloch_multivector
+
 def execute(circuit, shots):
     backend = Aer.get_backend("qasm_simulator")
     results = backend.run(transpile(circuit, backend), shots = shots).result()
     return results.get_counts()
+
+def visualize_the_system(circuit):
+    backend = Aer.get_backend("statevector_simulator")
+    results = backend.run(transpile(circuit, backend)).result()
+    display(plot_bloch_multivector(results.get_statevector()))
